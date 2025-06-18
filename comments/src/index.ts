8b7bc3a493e44347774e48796ca0c6842e766cd1
kastastin <kastastin.main@gmail.com>
2025-06-19 00:46:11 +0300

    add comments service with basic routes

---
+++ comments/src/index.ts
@@ -0,0 +1,35 @@
+import { randomBytes } from "crypto";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+app.use(express.json());
+
+const PORT = 4001;
+
+type Comment = {
+  id: string;
+  content: string;
+};
+
+const commentsByPostId: Record<string, Comment[]> = {};
+
+app.get("/posts/:id/comments", (req: Request, res: Response) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req: Request, res: Response) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
